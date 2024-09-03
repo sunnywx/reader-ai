1b107ae644bc1e7e3f9f5378e6a8ad77f9642473
sunnywx <iwisunny@gmail.com>
2024-09-03 14:06:52 +0800

    chore: book detail display page title

--- web/src/pages/book/[...slug].tsx
+++ web/src/pages/book/[...slug].tsx
@@ -1,19 +1,21 @@
-import {useRouter} from 'next/router'
-import {PdfViewer} from '@/components/pdf-viewer'
+import { useRouter } from "next/router";
+import { PdfViewer } from "@/components/pdf-viewer";
+import Head from "next/head";
 
-const apiPrefix='http://localhost:3001/get-raw-book'
+const apiPrefix = "http://localhost:3001/get-raw-book";
 
-export default function BookDetail(){
-  const {query}=useRouter()
-  const {slug=[]}=query as {slug: string[]}
+export default function BookDetail() {
+  const { query } = useRouter();
+  const { slug = [] } = query as { slug: string[] };
   // const fileName=slug.length > 0 ? slug[slug.length - 1] : ''
-  const filePath=slug.join('/')
+  const filePath = slug.join("/");
 
   return (
     <>
-      {/* book path: {filePath} */}
-
-      {filePath && <PdfViewer fileUrl={[apiPrefix, filePath].join('/')}/>}
+      <Head>
+        <title>{`Book: ${filePath}`}</title>
+      </Head>
+      {filePath && <PdfViewer fileUrl={[apiPrefix, filePath].join("/")} />}
     </>
-  )
+  );
 }
